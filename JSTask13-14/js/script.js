ce368094cb22c83b5ca5c9f6ad37c13bf346c9f4
Valerya <Lera1695@yandex.ru>
2016-05-02 12:55:23 +0300

    added JS13 home work

--- JSTask13-14/js/script.js
+++ JSTask13-14/js/script.js
@@ -1,23 +1,21 @@
 "use strict";
-var test = localStorage.getItem('test');
-    test = JSON.parse(test);
 $(function() {
     var tmplj = $('#blank').html();
     var data = [ 
     {
         question: 'Зачем используется ключевое слово var?',
         answer: ['Для вывода результатов программы на экран', 'Для вывода сообщения в отдельном окне',' Для создания новой переменной' ],
-        rideAnswer: [ 2 ]    
+        rightAnswer: [ 2 ]    
     },
     {
         question: 'Чем отличаются циклы while от do while?',
         answer: ['в javascript нет цикла do while', 'независимо от условия цикл while выполнится хотя бы раз','независимо от условия цикл do while выполнится хотя бы раз' ],
-        rideAnswer: [ 2 ]
+        rightAnswer: [ 2 ]
     },
     {
         question: 'Правильный вариант комментариев в javascript',
         answer: ['*закомментированный текст', '//закомментированный текст','/*закомментированный текст*/' ],
-        rideAnswer: [ 1, 2 ]
+        rightAnswer: [ 1, 2 ]
     }
     ];
  
@@ -28,54 +26,42 @@ $(function() {
     $('.wrapper__template').append(cont);
 });
 
+var test = localStorage.getItem('test');
+    test = JSON.parse(test);
+    
 $(function() { 
-    $(".form__button").click(function() {              
-    var $useAns = [];
+    $(".form__button").click(function(e) {              
+    var $useAns =[];
     var $a_r = 0;
     var $e = 0; 
-    var $otv;
+    e.preventDefault();
     $(".modal_form__text").empty();
-    for( var a = 0; a < 3; a++ )
+    
+   for( var a = 0; a < 3; a++ )
     {       
-        var $block_ = $("#bloc"+a+" input:checkbox:checked");
-        console.log('block number',$block_ );
-        $otv = test[a].rideAnswer;
-        for( var i = 0; i < $block_.length; i++ )
-        { 
-           $useAns[i] = $($block_[i]).val();
-           console.log('answer',$useAns[i]);
-        }
-        for( var zx=0; zx<$block_.length; zx++)
-        {                            
-            for( var x=0; x<$($(test[a].rideAnswer)).length; x++)
-            {  
-                console.log('use answer',$useAns);
-                console.log('correct answer',$otv[x]);
-                    if( $useAns[zx] == $otv[x] )
-                       //if($useAns.indexOf($otv) > -1)
-                      // if($useAns.includes($otv[x]))
-                       // if(find($useAns,$otv))
-                      //if($.inArray($otv[x],$useAns))
-                {
-                    $a_r++;
-                    console.log('in the if correct',$a_r++);
-                    break;
-                }
-                else{
-                    $e++;
-                    console.log('error',$e++)
-                    break;
-                }
-                    
-               }
-           }        
-    } 
+      $useAns[a] = $("#bloc"+a+" input:checkbox:checked").serializeArray();
+    }
+    $useAns.forEach(function (item, i) {
+       
+        item.forEach(function (item_arr, a) {
+       
+            if (item_arr.value == test[i].rightAnswer[a]) 
+            { 
+               $a_r++;
+            }
+            else{
+                $e++;
+            }
+
+        });
+    });
+
     if($e === 0)
     {
-       show("congratulations! " + $a_r, $useAns, $otv);
+       show("congratulations! " + $a_r, $useAns);
     }
     else{
-       show("Error: " + $e, $useAns, $otv);
+       show("Error: " + $e, $useAns);
     }         
   });
  });
@@ -95,7 +81,7 @@ initBlock("#bloc2",2);
 
 });        
 
-function show(res,arr,otv) { 
+function show(res,arr) { 
     $('.overlay').fadeIn(400,function(){ 
         $('.modal_form').css('display', 'block').animate({opacity: 1, top: '50%'}, 200);
         $('.modal_form .modal_form__text').append(res);
@@ -109,9 +95,7 @@ function show(res,arr,otv) {
     $(".modal__button").click(function() { 
         $(':input').removeAttr('checked'); 
         return res = null; 
-        return otv = null;
         return arr.length=0;
-        console.log(arr);
     });
     
 }
